f268d7fca9b47bc2f657b327cf32bcc4c4ca6396
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-17 11:55:46 -0300

    parte 11

--- backend/api/category.js
+++ backend/api/category.js
@@ -42,7 +42,7 @@ module.exports = app => {
   
       notExistsOrError(articles, 'Categoria possui artigos.')
   
-      const rowsDeleted = await app.db('categorias')
+      const rowsDeleted = await app.db('categories')
         .where({id: req.params.id}).del()
   
       existsOrError(rowsDeleted, 'Categoria não foi informada')
@@ -96,7 +96,23 @@ module.exports = app => {
       .catch(err => res.status(500).send(err))
   }
 
-   return {save, remove, get, getById}
+  const toTree = (categories, tree) => {
+    if(!tree) tree = categories.filter(c => !c.parentId)
+    tree = tree.map(parentNode => {
+      const isChild = node => node.parentId == parentNode.id
+      parentNode.children = toTree(categories, categories.filter(isChild))
+      return parentNode
+    })
+    return tree
+  }
+
+  const getTree = (req, res) => {
+    app.db('categories')
+      .then(categories => res.json(toTree(withPath(categories))))
+      .catch(err => res.status(500).send(err))
+  }   
+
+   return {save, remove, get, getById, getTree}
 }
 
 
