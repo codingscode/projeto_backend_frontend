b7125ebf12cdec083f0a66bc279d7fe2b57d2ec0
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-22 13:22:57 -0300

    parte 17

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
+const mongooose = require('mongoose')
+
+require('./config/mongodb')
+
 
 app.db = db
+app.mongoose = mongooose
 
 consign()
   .include('./config/passport.js')
