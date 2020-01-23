f40c88f785f323a5ac263300aff136d53445447d
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-23 10:50:16 -0300

    parte 18

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
 const app = require('express')()
 const consign = require('consign')
 const db = require('./config/db')
-const mongooose = require('mongoose')
+const mongoose = require('mongoose')
 
 require('./config/mongodb')
 
 
 app.db = db
-app.mongoose = mongooose
+app.mongoose = mongoose
 
 consign()
   .include('./config/passport.js')
   .then('./config/middlewares.js')
   .then('./api/validation.js')
   .then('./api')
+  .then('./schedule')
   .then('./config/routes.js')
   .into(app)
 
