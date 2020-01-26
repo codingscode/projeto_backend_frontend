dff455536e9afb71bd6e6f3e3daa8306f1bbe222
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-26 12:26:47 -0300

    f parte 2

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -1,20 +1,71 @@
 <template>
-    <div class="header"></div>
+    <div class="header">
+        <a class="toggle" @click="toggleMenu" v-if="!hideToggle">
+            <i class="fa la-lg" :class="icon"></i>
+        </a>
+        <h1 class="title">
+            {{title}}
+        </h1>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    computed: {
+        icon() {
+            return "fa-angle-left"
+        }
+    },
+    methods: {
+        toggleMenu() {
+           
+        }
+    }
 }
 </script>
 
 <style>
     .header {
         grid-area: header;
-        background: linear-gradient(to riht, rgb(155, 235, 155), white);
+        background: linear-gradient(to right, rgb(1, 112, 1), white);
 
         display: flex;
         justify-content: center;
         align-items: center;
     }
+
+    .title {
+        font-size: 1.2rem;
+        color: #fff;
+        font-weight: 100;
+        flex-grow: 1;
+        text-align: center;
+    }
+
+    .title a {
+        color:#FFF;
+        text-decoration: none;
+    }
+
+    header.header > a.toggle {
+        width: 60px;
+        height: 100%;
+        color: #fff;
+        justify-self: flex-start;
+        text-decoration: none;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    header.header > a.toggle:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+
 </style>
