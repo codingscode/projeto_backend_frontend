2cc26195ceac42597f127d7995fc2efa32eefa45
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-29 12:34:24 -0300

    f parte 6

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="admin-classes">
+        <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastro e Cia"/>
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+
+export default {
+   name: 'AdminPages',
+   components: {PageTitle}
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,13 @@ import App from './App'
 
 import './config/bootstrap'
 import store from './config/store'
+import router from './config/router'
 
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
