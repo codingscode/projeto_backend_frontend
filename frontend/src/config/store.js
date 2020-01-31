246327601ce13f955c00f10269eae5a354ad4799
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-30 21:00:01 -0300

    f parte 7

--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -20,7 +20,6 @@ export default new Vuex.Store({
                state.isMenuVisible = isVisible
             }
         
-            console.log('toggleMenu = ' + state.isMenuVisible)
        }
     }
 })
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,6 +10,10 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsYWRpbiBTYWxpbSIsImVtYWlsIjoiYWxhZGluQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODA0MDAwMjMsImV4cCI6MTU4MDY1OTIyM30.H6vuHpQOHHz0ArxokyQjgb4S5aC78SmABrLO_6BzzHA'
+
+
 new Vue({
   store,
   router,
