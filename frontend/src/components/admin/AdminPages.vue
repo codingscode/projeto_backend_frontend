99ddcbb533fd467ae5db585e94ddf6e2272be8ae
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-02 19:56:51 -0300

    f parte 8

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -1,15 +1,33 @@
 <template>
     <div class="admin-classes">
         <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastro e Cia"/>
+        <div class="admin-pages-tabs">
+           <b-card no-body>
+              <b-tabs card>
+                  <b-tab title="Artigos" active>
+                      <ArticleAdmin/>
+                  </b-tab>
+                  <b-tab title="Categorias">
+                      <CategoryAdmin/>
+                  </b-tab>
+                  <b-tab title="Usuários">
+                      <UserAdmin/>
+                  </b-tab>
+              </b-tabs>
+           </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
    name: 'AdminPages',
-   components: {PageTitle}
+   components: {PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin}
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsYWRpbiBTYWxpbSIsImVtYWlsIjoiYWxhZGluQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODA0MDAwMjMsImV4cCI6MTU4MDY1OTIyM30.H6vuHpQOHHz0ArxokyQjgb4S5aC78SmABrLO_6BzzHA'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsYWRpbiBTYWxpbSIsImVtYWlsIjoiYWxhZGluQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODA2ODI3MzIsImV4cCI6MTU4MDk0MTkzMn0.JCtYm_LB7OL0UyCciLCbr4m802129Nhgj-sR1IBtAGw'
 
 
 new Vue({
