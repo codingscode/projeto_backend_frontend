1309cd2ec75f1e2dcc751df7df57ede92e942087
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-16 20:39:45 -0300

    f parte 21

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // TEMPORARIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsYWRpbiBTYWxpbSIsImVtYWlsIjoiYWxhZGluQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODE0NTEwMDEsImV4cCI6MTU4MTcxMDIwMX0.xKjpISuLHPhz86BCt69qoGbj2J7be9h50cpQ_UKLPX4'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsYWRpbiBTYWxpbSIsImVtYWlsIjoiYWxhZGluQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1ODE3OTU0NzksImV4cCI6MTU4MjA1NDY3OX0.aTRS2LHqMFMAq5Sp_Z6f8NtgqDH2EgvH_MbERQnM1Ls'
 
 
 new Vue({
