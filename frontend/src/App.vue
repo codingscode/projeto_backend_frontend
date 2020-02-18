10095344d2c23125caca0897284c8237a83d6755
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-18 11:52:25 -0300

    f parte 26

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {baseApiUrl, userKey} from '@/global'
 import {mapState} from 'vuex'
 import Content from './components/template/Content'
 import Footer from './components/template/Footer'
@@ -18,7 +20,42 @@ import Menu from './components/template/Menu'
 export default {
 	name: "App",
 	components: {Content, Footer, Header, Menu},
-	computed: mapState(['isMenuVisible', 'user'])
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+				validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData) {
+				this.validatingToken = false
+				this.$router.push({name: 'auth'})
+				return
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if(res.data) {
+				this.$store.commit('setUser', userData)
+			}
+			else {
+				localStorage.removeItem(userKey)
+				this.$router.push({name: 'auth'})
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
